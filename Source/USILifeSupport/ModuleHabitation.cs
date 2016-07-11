3dc0e84de17a22c0934a7591ab49a21c2daee749
Bob Palmer <rbpalmer_nrt@hotmail.com>
2016-07-11 19:55:38 -0400

    Habitation multiplie now behaves as a base converter

--- Source/USILifeSupport/ModuleHabitation.cs
+++ Source/USILifeSupport/ModuleHabitation.cs
@@ -4,14 +4,43 @@ using USITools.Logistics;
 
 namespace LifeSupport
 {
-    public class ModuleHabitation : PartModule
+    public class ModuleHabitation : BaseConverter
     {
         [KSPField] 
-        public double KerbalMonths = 1;
+        public double BaseKerbalMonths = 1;
         [KSPField]
         public double CrewCapacity = 10;
         [KSPField] 
-        public double HabMultiplier = 0;
+        public double BaseHabMultiplier = 0;
+
+        [KSPField(isPersistant = true)]
+        public bool HabIsActive = false;
+
+        public double HabMultiplier
+        {
+            get
+            {
+                if (HabIsActive || HighLogic.LoadedSceneIsEditor)
+                    return BaseHabMultiplier;
+
+                return 0f;
+            }
+        }
+
+        public double KerbalMonths
+        {
+            get
+            {
+                if (HabIsActive || HighLogic.LoadedSceneIsEditor)
+                    return BaseKerbalMonths;
+                return 0f;
+            }
+        }
+
+        protected override void PostProcess(ConverterResults result, double deltaTime)
+        {
+            HabIsActive = result.TimeFactor > ResourceUtilities.FLOAT_TOLERANCE;
+        }
 
         public override string GetInfo()
         {
